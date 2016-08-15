38ef34a622126a72fbeae9a2c6e9e3f9fe1865b2
Heejong Lee <heejong@gmail.com>
2016-08-15 00:32:50 -0700

    update repository url for melpa

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -21,7 +21,7 @@
 (add-to-list 'package-archives
   '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
-  '("melpa" . "http://melpa.milkbox.net/packages/"))
+  '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 (mapc
