7dc9a138e0e36921c1b1ac1e6e3de7276e01e68b
Heejong Lee <heejong@gmail.com>
2021-12-18 19:22:07 -0800

    using use-package

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -16,43 +16,14 @@
   (setenv "PATH" (mapconcat 'identity exec-path path-separator))
 ))
 
-;; add marmalade, melpa repo
+;; initialize use-package
 (require 'package)
-(add-to-list 'package-archives
-  '("gnu" . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives
   '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
-
-(mapc
- (lambda (package)
-   (or (package-installed-p package)
-       (package-install package)))
- '(
-   auctex
-   auto-complete
-   clojure-mode
-   cider ;; clojure minor mode
-   expand-region
-   gnuplot
-   haskell-mode
-   helm
-   js2-mode
-   magit
-   markdown-mode
-   monokai-theme
-   multiple-cursors
-   neotree
-   projectile
-   proof-general
-   scala-mode
-   undo-tree
-   writegood-mode
-   yasnippet
-   zenburn-theme
-   zencoding-mode
-   )
- )
+(or (package-installed-p 'use-package)
+    (progn (package-refresh-contents)
+           (package-install 'use-package)))
 
 ;; general settings
 ;;(setq mac-command-modifier 'meta)
@@ -78,11 +49,14 @@
 (add-hook 'text-mode-hook 'turn-on-flyspell)
 (setq send-mail-function 'sendmail-send-it)
 
+;; org-mode settings
+(setq org-agenda-files '("~/Documents/org"))
+(define-key global-map (kbd "C-c a") 'org-agenda)
+
 ;; additional global shortcuts
 (define-key global-map (kbd "M-/") 'hippie-expand)
 (define-key global-map (kbd "C-x C-b") 'ibuffer)
 (define-key global-map (kbd "RET") 'newline-and-indent)
-(define-key global-map (kbd "C-c a") 'org-agenda)
 (define-key global-map (kbd "S-SPC") 'toggle-input-method)
 
 ;; resizing windows
@@ -92,50 +66,117 @@
 (global-set-key (kbd "M-<up>") 'enlarge-window)
 
 ;; autoload theme
-(load-theme 'monokai t)
-
-;; org-mode settings
-(setq org-agenda-files '("~/Documents/org"))
+(use-package monokai-theme
+  :ensure t
+  :config
+  (load-theme 'monokai t))
 
 ;; markdown-mode settings
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(use-package markdown-mode
+  :ensure t
+  :init
+  (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+  (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode)))
 
 ;; auctex settings
-(setq-default TeX-master nil)
-(setq TeX-PDF-mode t)
-(setq TeX-auto-save t)
-(setq TeX-parse-self t)
-(setq TeX-view-program-list '(("open" "open %o")))
-(setq TeX-view-program-selection '((output-pdf "open")))
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-(setq reftex-plug-into-AUCTeX t)
+(use-package tex
+  :ensure auctex
+  :init
+  (setq-default TeX-master nil)
+  (setq TeX-PDF-mode t)
+  (setq TeX-auto-save t)
+  (setq TeX-parse-self t)
+  (setq TeX-view-program-list '(("open" "open %o")))
+  (setq TeX-view-program-selection '((output-pdf "open")))
+  (setq reftex-plug-into-AUCTeX t)
+  (add-hook 'LaTeX-mode-hook 'turn-on-reftex))
 
 ;; expand-region settings
-(define-key global-map (kbd "C-=") 'er/expand-region)
+(use-package expand-region
+  :ensure t
+  :bind
+  ("C-=" . 'er/expand-region))
 
 ;; yasnippet settings
-;(yas-global-mode 1)
-(setq yas-prompt-functions
-  '(yas-dropdown-prompt yas-ido-prompt yas-completing-prompt yas-x-prompt yas-no-prompt))
+(use-package yasnippet
+  :ensure t
+  :init
+  (setq yas-prompt-functions
+        '(yas-dropdown-prompt
+          yas-ido-prompt
+          yas-completing-prompt
+          yas-x-prompt
+          yas-no-prompt)))
 
 ;; multiple-cursors
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(use-package multiple-cursors
+  :ensure t
+  :bind
+  ("C->" . 'mc/mark-next-like-this))
 
 ;; undo-tree mode
-(global-undo-tree-mode)
+(use-package undo-tree
+  :ensure t
+  :config
+  (global-undo-tree-mode))
 
 ;; projectile mode
-(projectile-global-mode)
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-global-mode))
 
 ;; helm settings
-(global-set-key (kbd "C-c h") 'helm-mini)
+(use-package helm
+  :ensure t
+  :bind
+  ("C-c h" . 'helm-mini))
 
 ;; magit shortcut
-(global-set-key (kbd "C-c g") 'magit-status)
+(use-package magit
+  :ensure t
+  :bind
+  ("C-c g" . 'magit-status))
 
-;; zencoding mode
-(add-hook 'sgml-mode-hook 'zencoding-mode)
+;; js2-mode
+(use-package js2-mode
+  :ensure t)
+
+;; writegood-mode
+(use-package writegood-mode
+  :ensure t)
+
+;; neotree
+(use-package neotree
+  :ensure t)
+
+;; scala-mode
+(use-package scala-mode
+  :ensure t)
+
+;; haskell-mode
+(use-package haskell-mode
+  :ensure t)
+
+;; auto-complete
+(use-package auto-complete
+  :ensure t)
+
+;; gnuplot
+(use-package gnuplot
+  :ensure t)
+
+;; proof-general
+(use-package proof-general
+  :ensure t)
+
+;; clojure-mode
+(use-package clojure-mode
+  :ensure t)
+
+;; cider
+(use-package cider
+  :ensure t)
 
 ;; auto-generated custom config to custom.el
 (setq custom-file "~/.emacs.d/custom.el")
